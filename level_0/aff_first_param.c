dff2c7860b3dc60e8060f1baf3c9429155a72f9a
Aida Sufaj <aidasufaj00@gmail.com>
2024-12-06 22:24:43 +0100

    ex3 level 0

--- level_0/aff_first_param.c
+++ level_0/aff_first_param.c
@@ -2,16 +2,20 @@
 
 // If the number of arguments is less than 1, the program displays \n.
 
+
 #include <unistd.h>
 
-int	main(int argc, char **argv)
+int main(int argc, char **argv)
 {
-	int i;
-
-	i = -1;
-	if (argc > 1)
-		while (argv[1][++i])
-			write(1, &argv[1][i], 1);
-	write(1, "\n", 1);
-	return (0);
+    if (argc > 1) // Check if there is at least one argument
+    {
+        int i = 0;
+        while (argv[1][i]) // Loop through the first argument
+        {
+            write(1, &argv[1][i], 1);
+            i++;
+        }
+    }
+    write(1, "\n", 1); // Always write a newline character
+    return (0);
 }
---
+++ level_0/aff_last_param.c
@@ -0,0 +1,22 @@
+/*Write a program that takes strings as arguments, and displays its last
+argument followed by a newline.
+
+If the number of arguments is less than 1, the program displays a newline.  */
+
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+     int i;
+     i = -1;
+
+     if (argc > 1)
+     {
+
+         while (argv[argc - 1][++i])
+         
+             write(1, &argv[argc - 1][i], 1);
+    }
+    write(1, "\n", 1);
+    return (0);
+}
